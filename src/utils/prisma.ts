c55f2d101495073c30336a5d70debf6eafdb0d5c
Stefaan <safwbdev@gmail.com>
2025-08-09 06:45:37 +0800

    prisma adjustments

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,5 +1,6 @@
 // import { PrismaClient } from "@prisma/client/extension";
 // import { PrismaClient } from "@/generated/prisma";
-import { PrismaClient } from "../generated/prisma";
+// import { PrismaClient } from "../generated/prisma";
+import { PrismaClient } from '@prisma/client'
 
 export const prisma = new PrismaClient();
